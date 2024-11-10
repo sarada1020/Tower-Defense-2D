4f116a8d86945991a3fa37cb285944bbb91fd39c
ipysk <sarahjuliaantunesv@gmail.com>
2024-11-10 09:36:00 -0300

    Inimigos, scripts + prefabs

---
+++ TowerDefense 2D/Assets/InimigoVermelho.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InimigoVermelho : InimigoBase
+{
+    protected override void ChegarNoFinalDoCaminho()
+    {
+        // Aqui você pode definir o que acontece quando o inimigo chega no final
+        Destroy(gameObject);
+    }
+}
+
