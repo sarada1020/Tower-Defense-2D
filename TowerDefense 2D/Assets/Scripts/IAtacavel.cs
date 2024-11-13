e60ff5a37cdcdbcd8a8eef61ba13a57acc7e5a96
ipysk <sarahjuliaantunesv@gmail.com>
2024-11-13 11:37:27 -0300

    IAtacavel comentado

--- TowerDefense 2D/Assets/Scripts/IAtacavel.cs
+++ TowerDefense 2D/Assets/Scripts/IAtacavel.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Interface IAtacavel: Define um contrato para objetos que podem receber dano.
 public interface IAtacavel
 {
+    // Método ReceberDano: Deve ser implementado por qualquer classe que herde esta interface.
+    // Representa a ação de receber dano, com a quantidade de dano especificada pelo parâmetro.
     void ReceberDano(int dano);
 }
-
