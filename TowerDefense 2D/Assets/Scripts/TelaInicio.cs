8a3e77077c539fe28194bc5776b2427b7b8e0e41
ipysk <sarahjuliaantunesv@gmail.com>
2024-11-13 14:17:39 -0300

    Update Tela Inicio

--- TowerDefense 2D/Assets/Scripts/TelaInicio.cs
+++ TowerDefense 2D/Assets/Scripts/TelaInicio.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Classe responsável pelo gerenciamento das interações na tela inicial do jogo.
 public class TelaInicio : MonoBehaviour
 {
+    // Método chamado quando o botão "Jogar" é clicado.
+    // Este método carrega a cena principal do jogo.
     public void Jogar()
     {
-        // Substitua "NomeDaCenaDoJogo" pelo nome da cena principal
+        // Carrega a cena chamada "SampleScene".
         SceneManager.LoadScene("SampleScene");
     }
 }
+
 
